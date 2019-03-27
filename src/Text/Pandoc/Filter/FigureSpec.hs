1ae941a06b036fbb7b146731a3238cbada18a8d3
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-03-26 21:14:04 -0400

    Formatting via hfmt
    Former-commit-id: 80c0551ec8bc161ce41fbd4b7108454ac655beab [formerly fb0880832f4f8b508d97d671c6b48a7c1b058a25]
    Former-commit-id: c219ba725eda43f67d62197fb342cc9b073c0b28

--- src/Text/Pandoc/Filter/FigureSpec.hs
+++ src/Text/Pandoc/Filter/FigureSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 {-|
 Module      : Text.Pandoc.Filter.Figure
 Copyright   : (c) Laurent P René de Cotret, 2019
@@ -10,52 +11,47 @@ Portability : portable
 This module defines types and functions that help
 with keeping track of figure specifications
 -}
+module Text.Pandoc.Filter.FigureSpec
+    ( FigureSpec(..)
+    , figurePath
+    , hiresFigurePath
+    , addPlotCapture
+    ) where
 
-module Text.Pandoc.Filter.FigureSpec (
-        FigureSpec (..)
-      , figurePath
-      , hiresFigurePath
-      , addPlotCapture
-) where
-
-import           Data.Hashable                ( Hashable
-                                              , hashWithSalt
-                                              , hash
-                                              )
+import           Data.Hashable                (Hashable, hash, hashWithSalt)
 import qualified Data.Text                    as T
 
-import           System.FilePath              ( FilePath
-                                              , addExtension
-                                              , (</>)
-                                              , replaceExtension
-                                              )
+import           System.FilePath              (FilePath, addExtension,
+                                               replaceExtension, (</>))
 
-import           Text.Pandoc.Filter.Scripting (PythonScript)
 import           Text.Pandoc.Definition       (Attr)
+import           Text.Pandoc.Filter.Scripting (PythonScript)
 
-data SaveFormat = PNG 
-                | PDF 
-                | SVG 
-                | JPG
+data SaveFormat
+    = PNG
+    | PDF
+    | SVG
+    | JPG
 
 -- | Datatype containing all parameters required
 -- to run pandoc-pyplot
 data FigureSpec = FigureSpec
-    { caption     :: String          -- ^ Figure caption.
-    , script      :: PythonScript    -- ^ Source code for the figure.
-    , directory   :: FilePath        -- ^ Directory where to save the file
-    , dpi         :: Int             -- ^ Dots-per-inch of figure
-    , blockAttrs  :: Attr            -- ^ Attributes not related to @pandoc-pyplot@ will be propagated.
+    { caption    :: String -- ^ Figure caption.
+    , script     :: PythonScript -- ^ Source code for the figure.
+    , directory  :: FilePath -- ^ Directory where to save the file
+    , dpi        :: Int -- ^ Dots-per-inch of figure
+    , blockAttrs :: Attr -- ^ Attributes not related to @pandoc-pyplot@ will be propagated.
     }
 
 instance Hashable FigureSpec where
-    hashWithSalt salt spec = 
+    hashWithSalt salt spec =
         hashWithSalt salt (caption spec, script spec, directory spec, dpi spec, blockAttrs spec)
 
 -- | Determine the path a figure should have.
 figurePath :: FigureSpec -> FilePath
-figurePath spec =  (directory spec </> stem spec)
-    where stem = flip addExtension ".png" . show . hash
+figurePath spec = (directory spec </> stem spec)
+  where
+    stem = flip addExtension ".png" . show . hash
 
 -- | The path to the high-resolution figure.
 hiresFigurePath :: FigureSpec -> FilePath
@@ -63,16 +59,22 @@ hiresFigurePath = flip replaceExtension ".hires.png" . figurePath
 
 -- | Modify a Python plotting script to save the figure to a filename.
 -- An additional file (with extension PNG) will also be captured.
-addPlotCapture :: FigureSpec        -- ^ Path where to save the figure
-               -> PythonScript      -- ^ Code block with added capture
+addPlotCapture ::
+       FigureSpec -- ^ Path where to save the figure
+    -> PythonScript -- ^ Code block with added capture
 addPlotCapture spec =
-    mconcat [ script spec
-            , "\nimport matplotlib.pyplot as plt"  -- Just in case
-            , plotCapture (figurePath spec) (dpi spec)
-            , plotCapture (hiresFigurePath spec) (minimum [200, 2 * dpi spec])
+    mconcat
+        [ script spec
+        , "\nimport matplotlib.pyplot as plt" -- Just in case
+        , plotCapture (figurePath spec) (dpi spec)
+        , plotCapture (hiresFigurePath spec) (minimum [200, 2 * dpi spec])
+        ]
+  where
+    plotCapture fname' dpi' =
+        mconcat
+            [ "\nplt.savefig("
+            , T.pack $ show fname' -- show is required for quotes
+            , ", dpi="
+            , T.pack $ show dpi'
+            , ")"
             ]
-    where
-        plotCapture fname' dpi' = mconcat [ "\nplt.savefig("
-                                        , T.pack $ show fname' -- show is required for quotes
-                                        , ", dpi=", T.pack $ show dpi'
-                                        , ")"]
