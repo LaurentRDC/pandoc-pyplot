d35da00debf5b516cc979d80d05f0d91bfd80fee
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-04-08 18:48:15 -0400

    Removed cleanup functions
    Former-commit-id: 92732428c57d2c6d9b6ca86e9398e6a864062250 [formerly 4d7578c4da61920090db1f67a8103ecfc6270472]
    Former-commit-id: 8ccc74a70b87228d7783e10fdedb72a9e545be0a

--- src/Text/Pandoc/Filter/Pyplot/Internal.hs
+++ src/Text/Pandoc/Filter/Pyplot/Internal.hs
@@ -11,13 +11,12 @@ This module re-exports internal pandoc-pyplot functionality.
 -}
 
 module Text.Pandoc.Filter.Pyplot.Internal (
-    , module Text.Pandoc.Filter.Pyplot.Configuration
+      module Text.Pandoc.Filter.Pyplot.Configuration
     , module Text.Pandoc.Filter.Pyplot.FigureSpec
     , module Text.Pandoc.Filter.Pyplot.Scripting
     , module Text.Pandoc.Filter.Pyplot.Types
  ) where
 
-import Text.Pandoc.Filter.Pyplot.Cleanup
 import Text.Pandoc.Filter.Pyplot.Configuration
 import Text.Pandoc.Filter.Pyplot.FigureSpec
 import Text.Pandoc.Filter.Pyplot.Scripting
