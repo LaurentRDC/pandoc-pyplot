b67b8746500f09ac634eb9cc727ac53cf5d6d171
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-06-03 09:29:55 -0400

    Using optparse-applicative for options parsing
    Former-commit-id: 5be1ec2fc2ca4fb097668fa204387f77637bfa98 [formerly 8f9b9f268cdd181acd50eff9a6238342bb2a8df1]
    Former-commit-id: 15cca3933628bf1b3d5c3670b52e60bc3710684d

--- executable/Main.hs
+++ executable/Main.hs
@@ -3,12 +3,16 @@
 
 module Main where
 
+import           Control.Applicative       ((<|>))
+
 import           Data.Default.Class        (def)
 import           Data.List                 (intersperse)
 import           Data.Monoid               ((<>))
 import qualified Data.Text                 as T
 
-import           System.Environment        (getArgs)
+import           Options.Applicative
+import qualified Options.Applicative.Help.Pretty as P
+
 import           System.Directory          (doesFileExist)
 import           System.IO.Temp            (writeSystemTempFile)
 
@@ -22,75 +26,87 @@ import           Paths_pandoc_pyplot       (version)
 
 import           ManPage                   (embedManualHtml)
 
-supportedSaveFormats :: [SaveFormat]
-supportedSaveFormats = enumFromTo minBound maxBound
-
-manualHtml :: T.Text
-manualHtml = T.pack $(embedManualHtml)
-
-data Flag = Help
-          | Version
-          | Formats
-          | Manual
-    deriving (Eq)
-
-parseFlag :: [String] -> Maybe Flag
-parseFlag s
-    | head s `elem` ["-h", "--help"]       = Just Help
-    | head s `elem` ["-v", "--version"]    = Just Version
-    | head s `elem` ["-f", "--formats"]    = Just Formats
-    | head s `elem` ["-m", "--manual"]     = Just Manual
-    | otherwise                            = Nothing -- This is the regular input from pandoc
-
-flagAction :: Flag -> IO ()
-flagAction f
-    | f == Help    = showHelp
-    | f == Version = showVersion
-    | f == Formats = showFormats
-    | f == Manual  = showManual
-    | otherwise    = error "Unknown flag"
-    where   
-        showHelp    = putStrLn help
-        showVersion = putStrLn (V.showVersion version)
-        showFormats = putStrLn . mconcat . intersperse ", " . fmap show $ supportedSaveFormats
-        showManual  = writeSystemTempFile "pandoc-pyplot-manual.html" (T.unpack manualHtml) 
-                        >>= \fp -> openBrowser ("file:///" <> fp) >> return ()
-
-help :: String
-help =
-    "\n\
-    \\n\
-    \   usage: pandoc-pyplot [-h, --help] [-v, --version] [-f, --formats] [-m, --manual]\n\
-    \\n\
-    \   This pandoc filter generates plots from Python code blocks using Matplotlib.\n\
-    \   This allows to keep documentation and figures in perfect synchronicity.\n\
-    \\n\
-    \   Optional arguments:\n\
-    \       -h, --help       Show this help message and exit.\n\
-    \       -v, --version    Show version number and exit.\n\
-    \       -f, --formats    Show supported output figure formats and exit.\n\
-    \       -m, --manual     Open the manual page in the default web browser and exit.\n\
-    \\n\
-    \   Example usage with pandoc: \n\
-    \\n\
-    \       > pandoc --filter pandoc-pyplot input.md --output output.html\n\
-    \\n\
-    \   If you use pandoc-pyplot in combination with other filters, you probably want\n\
-    \   to run pandoc-pyplot first. Here is an example with pandoc-crossref: \n\
-    \\n\
-    \       > pandoc --filter pandoc-pyplot --filter pandoc-crossref -i input.md -o output.pdf\n\
-    \\n\
-    \   More information can be found via the manual (pandoc-pyplot --manual) or the\n\ 
-    \   repository README, located at \n\
-    \       https://github.com/LaurentRDC/pandoc-pyplot\n"
-
 main :: IO ()
 main = do
     configExists <- doesFileExist ".pandoc-pyplot.yml"
     config <- if configExists
                 then configuration ".pandoc-pyplot.yml"
                 else def
-
-    getArgs >>= \args -> case parseFlag args of
+    
+    options <- execParser opts
+    case options of
         Just f  -> flagAction f
         Nothing -> toJSONFilter (plotTransformWithConfig config)
+        
+
+data Flag = Version
+          | Formats
+          | Manual
+    deriving (Eq)
+
+versionP :: Parser (Maybe Flag)
+versionP = flag Nothing (Just Version) 
+    (long "version" 
+    <> short 'v' 
+    <> help "Show version number and exit."
+    )
+
+formatsP :: Parser (Maybe Flag)
+formatsP = flag Nothing (Just Formats) 
+    (long "formats" 
+    <> short 'f' 
+    <> help "Show supported output figure formats and exit."
+    )
+
+manualP :: Parser (Maybe Flag)
+manualP = flag Nothing (Just Manual) 
+    (long "manual" 
+    <> short 'm' 
+    <> help "Open the manual page in the default web browser and exit."
+    )
+
+optionsParser :: Parser (Maybe Flag)
+optionsParser = versionP <|> formatsP <|> manualP
+
+opts :: ParserInfo (Maybe Flag)
+opts = info (optionsParser <**> helper)
+    (fullDesc
+    <> progDesc "This pandoc filter generates plots from Python code blocks using Matplotlib. This allows to keep documentation and figures in perfect synchronicity."
+    <> header "pandoc-pyplot - generate Matplotlib figures directly in documents."
+    <> footerDoc (Just footer')
+    )
+
+flagAction :: Flag -> IO ()
+flagAction f
+    | f == Version = showVersion
+    | f == Formats = showFormats
+    | f == Manual  = showManual
+    | otherwise    = error "Unknown flag"
+    where   
+        showVersion = putStrLn (V.showVersion version)
+        showFormats = putStrLn . mconcat . intersperse ", " . fmap show $ supportedSaveFormats
+        showManual  = writeSystemTempFile "pandoc-pyplot-manual.html" (T.unpack manualHtml) 
+                        >>= \fp -> openBrowser ("file:///" <> fp) >> return ()
+
+supportedSaveFormats :: [SaveFormat]
+supportedSaveFormats = enumFromTo minBound maxBound
+
+manualHtml :: T.Text
+manualHtml = T.pack $(embedManualHtml)
+
+-- | Use Doc type directly because of newline formatting
+footer' :: P.Doc
+footer' = mconcat [
+        P.text "Example usage with pandoc:"
+    , P.line, P.line
+    , P.indent 4 $ P.string "> pandoc --filter pandoc-pyplot input.md --output output.html"
+    , P.line, P.line
+    , P.text "If you use pandoc-pyplot in combination with other filters, you probably want to run pandoc-pyplot first. Here is an example with pandoc-crossref:"
+    , P.line, P.line
+    , P.indent 4 $ P.string "> pandoc --filter pandoc-pyplot --filter pandoc-crossref -i input.md -o output.pdf"
+    , P.line, P.line
+    , P.text "More information can be found via the manual (pandoc-pyplot --manual) or the repository README, located at"
+    , P.line
+    , P.indent 4 $ P.text "https://github.com/LaurentRDC/pandoc-pyplot"
+    , P.line
+    ]
