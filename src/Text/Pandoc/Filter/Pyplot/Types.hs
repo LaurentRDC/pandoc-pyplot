0a62dac38f3ebda6538ffd8d428e907862a519ee
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-11-05 08:35:27 -0500

    [skip ci] Documentation improvement

--- src/Text/Pandoc/Filter/Pyplot/Types.hs
+++ src/Text/Pandoc/Filter/Pyplot/Types.hs
@@ -221,10 +221,10 @@ data FigureSpec = FigureSpec
     , script       :: PythonScript     -- ^ Source code for the figure.
     , saveFormat   :: SaveFormat       -- ^ Save format of the figure.
     , directory    :: FilePath         -- ^ Directory where to save the file.
-    , dpi          :: Int              -- ^ Dots-per-inch of figure.
+    , dpi          :: Int              -- ^ Dots-per-inch of figure. This option only affects the Matplotlib backend.
     , renderingLib :: RenderingLibrary -- ^ Rendering library.
-    , tightBbox    :: Bool             -- ^ Enforce tight bounding-box with @bbox_inches="tight"@.
-    , transparent  :: Bool             -- ^ Make figure background transparent.
+    , tightBbox    :: Bool             -- ^ Enforce tight bounding-box with @bbox_inches="tight"@. This option only affects the Matplotlib backend.
+    , transparent  :: Bool             -- ^ Make figure background transparent. This option only affects the Matplotlib backend.
     , blockAttrs   :: Attr             -- ^ Attributes not related to @pandoc-pyplot@ will be propagated.
     } deriving Generic
 
